d83bebf25621ae66150336443918bcb19804be1e
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-01-28 17:38:54 +0500

    Update consts.dart

--- lib/Utils/consts.dart
+++ lib/Utils/consts.dart
@@ -3,6 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 final userRef = FirebaseFirestore.instance.collection('users');
+final announcementsRef = FirebaseFirestore.instance.collection('announcements');
+final commentsRef = FirebaseFirestore.instance.collection('comments');
+final chatRoomRef = FirebaseFirestore.instance.collection('chatRoom');
+final chatListRef = FirebaseFirestore.instance.collection('chatLists');
 
 AppUserModel? currentUser;
 TextStyle titleTextStyle({double fontSize = 25, Color color = Colors.black}) {
