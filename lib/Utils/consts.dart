bbe6ec6dc4d30b93f13ffe21f7d75028de7fafbf
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-01-28 17:38:23 +0500

    Update consts.dart

--- lib/Utils/consts.dart
+++ lib/Utils/consts.dart
@@ -1,5 +1,8 @@
 import 'package:bull_signal/Models/users.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+final userRef = FirebaseFirestore.instance.collection('users');
 
 AppUserModel? currentUser;
 TextStyle titleTextStyle({double fontSize = 25, Color color = Colors.black}) {
